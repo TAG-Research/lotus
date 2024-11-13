11425fa555bda3adbe488ced32778d4351521512
Harshit Gupta <gharshit@stanford.edu>
2024-11-13 00:50:04 -0800

    add example

---
+++ examples/multimodal_data/filter.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from torchvision import datasets
+import lotus
+from lotus.models import LM
+from lotus.dtype_extensions import ImageArray
+
+lm = LM(model="gpt-4o-mini")
+lotus.settings.configure(lm=lm)
+
+mnist_data = datasets.MNIST(root='mnist_data', train=True, download=True, transform=None)
+
+images = [image for image, _ in mnist_data]
+labels = [label for _, label in mnist_data]
+
+df = pd.DataFrame({
+    "image": ImageArray(images),
+    "label": labels
+})
+
+df.sem_filter("{image} represents number 1")
